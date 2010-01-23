bd1e575e55c83cfce737caa863200b31c18ccbce
Alex Gordon <anything@fileability.net>
2010-01-23 22:44:29 +0000

    Project added

---
+++ Ingredients_AppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  Ingredients_AppDelegate.h
+//  Ingredients
+//
+//  Created by Alex Gordon on 23/01/2010.
+//  Copyright Fileability 2010 . All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Ingredients_AppDelegate : NSObject 
+{
+    NSWindow *window;
+    
+    NSPersistentStoreCoordinator *persistentStoreCoordinator;
+    NSManagedObjectModel *managedObjectModel;
+    NSManagedObjectContext *managedObjectContext;
+}
+
+@property (nonatomic, retain) IBOutlet NSWindow *window;
+
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:sender;
+
+@end
---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Ingredients
+//
+//  Created by Alex Gordon on 23/01/2010.
+//  Copyright Fileability 2010. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
