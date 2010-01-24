2872f8947c4b437854d0e76fb067c9041627cb0f
Alex Gordon <anything@fileability.net>
2010-01-24 02:18:34 +0000

    ...

---
+++ IGKWindowController.h
@@ -0,0 +1,16 @@
+//
+//  IGKWindowController.h
+//  Ingredients
+//
+//  Created by Alex Gordon on 23/01/2010.
+//  Copyright 2010 Fileability. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface IGKWindowController : NSWindowController {
+
+}
+
+@end
---
+++ IGKWindowController.m
@@ -0,0 +1,19 @@
+//
+//  IGKWindowController.m
+//  Ingredients
+//
+//  Created by Alex Gordon on 23/01/2010.
+//  Copyright 2010 Fileability. All rights reserved.
+//
+
+#import "IGKWindowController.h"
+
+
+@implementation IGKWindowController
+
+- (NSString *)windowNibName
+{
+	return @"CHDocumentationBrowser";
+}
+
+@end
