bfef8a62f17c06e9854ce59e806d369770b7f588
Alex Gordon <anything@fileability.net>
2010-07-12 17:39:27 +0100

    Now with the added feature of compiling without errors!

--- IGKWindowController.h
+++ IGKWindowController.h
@@ -174,6 +174,7 @@ typedef enum {
 @property (readonly) NSString *sideSearchQuery;
 
 + (NSImage *)iconImageForURL:(NSURL *)url;
++ (IGKHTMLDisplayTypeMask)tableOfContentsMenuItemToMask:(NSInteger)tag;
 
 - (IBAction)noselectionSearchField:(id)sender;
 
