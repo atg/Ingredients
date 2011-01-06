58a506e93763209c4b5d980b922e7100c706a407
Alex Gordon <alextgordon@gmail.com>
2011-01-06 16:04:58 +0000

    Changed bwtoolkit to be app store compliant

--- BWToolkitFramework.framework/Versions/A/Headers/BWGradientBox.h
+++ BWToolkitFramework.framework/Versions/A/Headers/BWGradientBox.h
@@ -8,17 +8,22 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface BWGradientBox : NSView 
-{
+@interface BWGradientBox : NSView {
+
 	NSColor *fillStartingColor, *fillEndingColor, *fillColor;
-	NSColor *topBorderColor, *bottomBorderColor;
-	float topInsetAlpha, bottomInsetAlpha;
-	
-	BOOL hasTopBorder, hasBottomBorder, hasGradient, hasFillColor;
+
+	NSColor *topBorderColor, *rightBorderColor, *bottomBorderColor, *leftBorderColor;
+
+	float topInsetAlpha, rightInsetAlpha, bottomInsetAlpha, leftInsetAlpha;
+	BOOL hasTopBorder, hasRightBorder, hasBottomBorder, hasLeftBorder;
+	BOOL hasGradient, isVertical;
+
 }
 
-@property (nonatomic, retain) NSColor *fillStartingColor, *fillEndingColor, *fillColor, *topBorderColor, *bottomBorderColor;
-@property float topInsetAlpha, bottomInsetAlpha;
-@property BOOL hasTopBorder, hasBottomBorder, hasGradient, hasFillColor;
+@property (nonatomic, retain) NSColor *fillStartingColor, *fillEndingColor, *fillColor;
+@property (nonatomic, retain) NSColor *topBorderColor, *rightBorderColor, *bottomBorderColor, *leftBorderColor;
+@property float topInsetAlpha, rightInsetAlpha, bottomInsetAlpha, leftInsetAlpha;
+@property BOOL hasTopBorder, hasRightBorder, hasBottomBorder, hasLeftBorder;
+@property BOOL hasGradient, isVertical;
 
 @end
